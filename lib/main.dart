dc5ec38eb9def85b426506e8413d93b79cc57742
Arie Prasetyo <arie@local.mac.15>
2019-12-02 14:24:31 +0700

    adding component

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:flutter/material.dart';
+import 'randomwords.dart';
+
+void main() => runApp(MyApp());
+
+// #docregion MyApp
+class MyApp extends StatelessWidget {
+  // #docregion build
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      home: RandomWords(),
+    );
+  }
+
+}
